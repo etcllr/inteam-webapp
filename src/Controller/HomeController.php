8884a7ddcfb92f6c606fbe788c9b1b03f0f2e767
Etienne <cellier.etienne@gmail.com>
2023-02-22 13:39:52 +0100

    login

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,14 +3,30 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'home')]
-    public function index()
+    #[Route('/admin', name: 'home-admin')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function indexAdmin()
     {
-        return $this->render('home.html.twig');
+            return $this->render('home-admin.html.twig');
+    }
+
+    #[Route('/maintainer', name: 'home-maintainer')]
+    #[IsGranted('ROLE_MAINTAINER')]
+    public function indexMaintainer()
+    {
+        return $this->render('home-maintainer.html.twig');
+    }
+
+    #[Route('/', name: 'home-user')]
+    #[IsGranted('ROLE_USER')]
+    public function indexUser()
+    {
+        return $this->render('home-user.html.twig');
     }
 }
